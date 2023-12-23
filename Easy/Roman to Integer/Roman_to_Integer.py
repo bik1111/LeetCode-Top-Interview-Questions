df5f026fcf3c1d1703a5fa9b4f29bfc6d987d9da
bik1111 <bik1111@khu.ac.kr>
2023-12-23 14:31:25 +0900

    Easy : Roman to Integer

---
+++ Easy/Roman to Integer/Roman_to_Integer.py
@@ -0,0 +1,18 @@
+#url : https://leetcode.com/problems/roman-to-integer/?envType=featured-list&envId=top-interview-questions?envType=featured-list&envId=top-interview-questions
+
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        m = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000 }
+
+        ans = 0
+
+        for i in range(len(s)):
+            if i < len(s) -1 and m[s[i]] < m[s[i+1]]:
+                ans -= m[s[i]]
+            else:
+                ans += m[s[i]]
+
+
+myObj = Solution()
+result = myObj.romanToInt('MCMXCIV')
+print(result)
